3c94b5454166cecad10608fbca77e9f6b0627db8
jimly <jimly1032@gmail.com>
2012-05-01 23:53:03 +0800

    端口号与授权的端口号不符合，已改正

--- routes/login.js
+++ routes/login.js
@@ -5,7 +5,7 @@ exports.login=function(req,res){
 		client_id:'4048601347',
 		client_secret:'31075b7a8e87ff11d5ab1393623db2db',
 //		redirect_uri:'http://tddps.cloudfoundry.com/login',
-		redirect_uri:'http://127.0.0.1:30/login',
+		redirect_uri:'http://127.0.0.1:3000/login',
 		response_type:'',
 		display:'popup',
 		room:''
